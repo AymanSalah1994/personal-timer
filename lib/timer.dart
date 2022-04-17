39e8f2e1717a23d08477980fd6a741eb8bfd47a5
AymanSalah1994 <80077545+AymanSalah1994@users.noreply.github.com>
2022-04-17 13:05:14 +0200

    Added Start work Function

--- lib/timer.dart
+++ lib/timer.dart
@@ -2,22 +2,49 @@ import 'dart:async';
 import 'timer_model.dart';
 
 class CountDownTimer {
+  int work = 30;
   double _radius = 1;
   bool _isActive = true;
   Timer? timer;
   Duration? _time;
-  // Remaining time  
+  // Remaining time
   Duration? _fullTime;
 
-  
   String returnTime(Duration t) {
     // Takes the Time and Transforms it into a String
     // And then returns it to be Shown as a Text
-    // 
-    String  minutes = (t.inMinutes < 10) ? '0' + t.inMinutes.toString() : t.inMinutes.toString();
+    //
+    String minutes = (t.inMinutes < 10)
+        ? '0' + t.inMinutes.toString()
+        : t.inMinutes.toString();
     int timeSeconds = t.inSeconds - (t.inMinutes * 60);
-    //  This Will return ommited secodns 
-    String seconds = (timeSeconds < 10) ? '0' + timeSeconds.toString() : timeSeconds.toString();
+    //  This Will return ommited secodns
+    String seconds = (timeSeconds < 10)
+        ? '0' + timeSeconds.toString()
+        : timeSeconds.toString();
     return minutes + ':' + seconds;
+  }
+
+// Object . stream ,, you List to it When there is a New Data  ;
+
+  Stream<TimerModel> stream() async* {
+    yield* Stream.periodic(Duration(seconds: 1), (int a) {
+      String time;
+      if (this._isActive) {
+        _time = _time! - Duration(seconds: 1);
+        _radius = _time!.inSeconds / _fullTime!.inSeconds;
+        if (_time!.inSeconds <= 0) {
+          _isActive = false;
+        }
+      }
+      time = returnTime(_time!);
+      return TimerModel(percent: _radius, time: time);
+    });
+  }
+
+  void startwork() {
+    _radius = 1;
+    _time = Duration(minutes: work, seconds: 0);
+    _fullTime = _time;
   }
 }
--- lib/timer_model.dart
+++ lib/timer_model.dart
@@ -1,6 +1,6 @@
 class TimerModel {
   String? time;
-  double? percent; 
+  double? percent;
   //  Now we say they Both can be Null with this Question Mark
   //  ? means it can be null
   //  ! means it can not be null
